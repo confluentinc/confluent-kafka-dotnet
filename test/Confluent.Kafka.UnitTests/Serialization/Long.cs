e0131db7f8c3a9d867c8184fdd66870c8ba7cdd7
Glen Thomas <glen.thomas@outlook.com>
2017-03-22 13:02:59 +0000

    Added unit test for Long serialization

---
+++ test/Confluent.Kafka.UnitTests/Serialization/Long.cs
@@ -0,0 +1,17 @@
+﻿using Confluent.Kafka.Serialization;
+using Xunit;
+
+namespace Confluent.Kafka.UnitTests.Serialization
+{
+    public class LongTests
+    {
+        [Fact]
+        public void SerializeDeserialize()
+        {
+            Assert.Equal(0, new LongDeserializer().Deserialize(new LongSerializer().Serialize(0)));
+            Assert.Equal(100L, new LongDeserializer().Deserialize(new LongSerializer().Serialize(100L)));
+            Assert.Equal(9223372036854775807, new LongDeserializer().Deserialize(new LongSerializer().Serialize(9223372036854775807)));
+            Assert.Equal(-9223372036854775807, new LongDeserializer().Deserialize(new LongSerializer().Serialize(-9223372036854775807)));
+        }
+    }
+}
