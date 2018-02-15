e708d353bec0dbaba295b6721f91cbb131823a85
Matt Howlett <matt@confluent.io>
2018-02-15 15:21:25 -0800

    fix Partition ToString method

--- src/Confluent.Kafka/Partition.cs
+++ src/Confluent.Kafka/Partition.cs
@@ -206,9 +206,9 @@ namespace Confluent.Kafka
             switch (Value)
             {
                 case RD_KAFKA_PARTITION_UA:
-                    return $"Unknown [{RD_KAFKA_PARTITION_UA}]";
+                    return $"[Any]";
                 default:
-                    return Value.ToString();
+                    return $"[{Value}]";
             }
         }
     }
