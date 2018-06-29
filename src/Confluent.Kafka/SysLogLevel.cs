e07eb89de006c2bd3975e6dd86cab7705c7c65b9
Matt Howlett <matt@confluent.io>
2018-06-29 14:57:41 -0700

    fixed formatting

--- src/Confluent.Kafka/SysLogLevel.cs
+++ src/Confluent.Kafka/SysLogLevel.cs
@@ -1,41 +1,48 @@
 namespace Confluent.Kafka
 {
     /// <summary>
-    ///  Represents enumeration with levels comming from syslog(3)
+    ///     Represents enumeration with levels comming from syslog(3)
     /// </summary>
     public enum SyslogLevel
     {
         /// <summary>
-        ///  debug-level message
-        /// </summary>
-        Debug = 7,
-        /// <summary>
-        ///  informational message
-        /// </summary>
-        Info = 6,
-        /// <summary>
-        ///  normal, but significant condition
-        /// </summary>
-        Notice = 5,
-        /// <summary>
-        ///  warning condition
-        /// </summary>
-        Warning = 4,
-        /// <summary>
-        ///  error condition
-        /// </summary>
-        Error = 3,
-        /// <summary>
-        ///  critical condition
-        /// </summary>
-        Critical = 2,
-        /// <summary>
-        ///  action must be take immediately
-        /// </summary>
-        Alert = 1,
-        /// <summary>
-        ///  system is unstable
+        ///     System is unusable.
         /// </summary>
         Emergency = 0,
+
+        /// <summary>
+        ///     Action must be take immediately
+        /// </summary>
+        Alert = 1,
+
+        /// <summary>
+        ///     Critical condition.
+        /// </summary>
+        Critical = 2,
+
+        /// <summary>
+        ///     Error condition.
+        /// </summary>
+        Error = 3,
+
+        /// <summary>
+        ///     Warning condition.
+        /// </summary>
+        Warning = 4,
+
+        /// <summary>
+        ///     Normal, but significant condition.
+        /// </summary>
+        Notice = 5,
+        
+        /// <summary>
+        ///     Informational message.
+        /// </summary>
+        Info = 6,
+        
+        /// <summary>
+        ///     Debug-level message.
+        /// </summary>
+        Debug = 7
     }
 }
