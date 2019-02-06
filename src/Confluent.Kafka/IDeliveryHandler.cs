confluentinc/confluent-kafka-dotnet

commit 6c3df62e6dbe419612f518eb5bf2d2b4ba703a14

remove non-serializing `Producer` and `Consumer` classes (#760)
* added SchemaRegistryConfigBasicAuthCredentialsSource

* review changes

* actually ran the integration tests this time

* badly formatted xml comment fix

* remove base classes + byte[] clients

* removed ProducerCore (base)

* delivery handler -> internal

* internal -> private

* removed pointless summaries

* remove out of date commnet

src/Confluent.Kafka/IDeliveryHandler.cs
@@ -1,40 +1,0 @@
-// Copyright 2016-2017 Confluent Inc., 2015-2016 Andreas Heider
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// Derived from: rdkafka-dotnet, licensed under the 2-clause BSD License.
-//
-// Refer to LICENSE for more information.
-
-using System;
-
-
-namespace Confluent.Kafka
-{
-    /// <summary>
-    ///     Defines an interface for handling Producer delivery reports.
-    /// </summary>
-    /// <remarks>
-    ///     You will typically never need to implement this interface.
-    /// </remarks>
-    public interface IDeliveryHandler
-    {        
-        /// <summary>
-        ///     Called when a delivery report is available.
-        /// </summary>
-        /// <param name="deliveryReport">
-        ///     The delivery report.
-        /// </param>
-        void HandleDeliveryReport(DeliveryReport deliveryReport);
-    }
-}

test/Confluent.Kafka.UnitTests/Producer.cs
@@ -31,7 +31,7 @@ namespace Confluent.Kafka.UnitTests
             // respective config key.
             var configWithNullValue = new ProducerConfig();
             configWithNullValue.Set("sasl.password", null);
-            var e = Assert.Throws<ArgumentException>(() => { var c = new ProducerBuilder(configWithNullValue).Build(); });
+            var e = Assert.Throws<ArgumentException>(() => { var c = new ProducerBuilder<byte[], byte[]>(configWithNullValue).Build(); });
             Assert.Contains("sasl.password", e.Message);
         }
     }

commit a6ffbb39782fb948ab0ab0df2708a305ca2f704d

fix regression introduced in #760

src/Confluent.Kafka/IDeliveryHandler.cs
@@ -1,0 +1,24 @@
+// Copyright 2019 Confluent Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Refer to LICENSE for more information.
+
+
+namespace Confluent.Kafka
+{
+    internal interface IDeliveryHandler
+    {
+        void HandleDeliveryReport(DeliveryReport<Null, Null> deliveryReport);
+    }
+}

src/Confluent.Kafka/Producer.cs
@@ -40,11 +40,6 @@ namespace Confluent.Kafka
             public Action<string> statisticsHandler;
         }
 
-        private interface IDeliveryHandler
-        {
-            void HandleDeliveryReport(DeliveryReport<Null, Null> deliveryReport);
-        }
-
         private ISerializer<TKey> keySerializer;
         private ISerializer<TValue> valueSerializer;
         private IAsyncSerializer<TKey> asyncKeySerializer;