0dbf9a582e7adcb16321e5557df58193af8bb0f9
Matt Kellogg <matthew.kellogg@gmail.com>
2019-02-11 12:29:36 -0700

    fixed error with schema registry config in .net 461 (bad assumption on Convert.ToString

---
+++ test/Confluent.SchemaRegistry.UnitTests-net461/CachedSchemaRegistryClient.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.SchemaRegistry;
+using Xunit;
+
+namespace Confluent.SchemaRegistry.UnitTests_net461
+{
+    
+    public class CachedSchemaRegistryClientTests
+    {
+        [Fact]
+        public void ConvertToStringInNet461DoesNotReturnEmptyStringForNullValues()
+        {
+            var schemaRegistryClient = new CachedSchemaRegistryClient(new SchemaRegistryConfig()
+            {
+               SchemaRegistryUrl = "http://irrelevanthost:80",
+            });
+
+            Assert.NotNull(schemaRegistryClient);
+        }
+    }
+}
