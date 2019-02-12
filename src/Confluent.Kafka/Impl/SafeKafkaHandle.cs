90a1a81183a3adefc6b04f7f9df8ab41812224c7
Matt Howlett <matt@confluent.io>
2019-02-11 17:27:25 -0800

    remove now unneeded using statements

--- src/Confluent.Kafka/Impl/SafeKafkaHandle.cs
+++ src/Confluent.Kafka/Impl/SafeKafkaHandle.cs
@@ -19,7 +19,6 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
-using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
