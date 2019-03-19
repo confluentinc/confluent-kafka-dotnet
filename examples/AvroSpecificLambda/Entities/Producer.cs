c2bb6052217ae8795231b0d14a8902fe3c85ca3f
Nick Pirocanac <nick@zaphop.com>
2019-03-19 09:28:50 -0500

    Adding AvroSpecificLambda example.

---
+++ examples/AvroSpecificLambda/Entities/Producer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvroSpecificLambda.Entities
+{
+    public class Producer
+    {
+        public string BootstrapServers { get; set; }
+        public string SchemaRegistryUrl { get; set; }
+        public string TopicName { get; set; }
+    }
+}
