4db8b77c898a43f30223121b434479c4dc16f411
Nick Pirocanac <nick@zaphop.com>
2019-03-19 12:21:20 -0500

    Downgrading AvroSpecificWebApi to .net core 2.1

--- examples/AvroSpecificWebApi/Startup.cs
+++ examples/AvroSpecificWebApi/Startup.cs
@@ -25,7 +25,7 @@ namespace AvroSpecificWebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddSwaggerGen(c =>
             {
---
+++ examples/AvroSpecificWebApiNet47/Areas/HelpPage/ModelDescriptions/ModelDescription.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace AvroSpecificWebApi.Areas.HelpPage.ModelDescriptions
+{
+    /// <summary>
+    /// Describes a type model.
+    /// </summary>
+    public abstract class ModelDescription
+    {
+        public string Documentation { get; set; }
+
+        public Type ModelType { get; set; }
+
+        public string Name { get; set; }
+    }
+}
