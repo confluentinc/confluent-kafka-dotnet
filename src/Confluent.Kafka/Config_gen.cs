91cb778a401c725ead78844a9be835b5d93679c7
Matt Howlett <matt.howlett@gmail.com>
2019-12-03 16:15:31 -0800

    -> v1.3.0 (#1132)
    accepting your pre-review @edenhill - these changes conform to what you've previously noted shouldn't require a review.

--- src/Confluent.Kafka/Config_gen.cs
+++ src/Confluent.Kafka/Config_gen.cs
@@ -1,4 +1,4 @@
-// *** Auto-generated from librdkafka v1.3.0-RC3 *** - do not modify manually.
+// *** Auto-generated from librdkafka v1.3.0 *** - do not modify manually.
 //
 // Copyright 2018 Confluent Inc.
 //
