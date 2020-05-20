b3e4419a9fa3eca327c7c069d7c4942cfb250096
Anton Denisov <andenisov@ozon.ru>
2020-05-20 23:35:20 +0300

    [871] SafeKafkaHandle: ~fix NRE in OauthBearerSetToken

--- src/Confluent.Kafka/Impl/SafeKafkaHandle.cs
+++ src/Confluent.Kafka/Impl/SafeKafkaHandle.cs
@@ -1558,10 +1558,12 @@ namespace Confluent.Kafka.Impl
 
         internal void OauthBearerSetToken(string tokenValue, long lifetimeMs, string principalName, string[] extensions)
         {
+            if (tokenValue == null) throw new ArgumentNullException(nameof(tokenValue));
+
             var errorStringBuilder = new StringBuilder(Librdkafka.MaxErrorStringLength);
             var errorCode = Librdkafka.oauthbearer_set_token(handle,
                 tokenValue, lifetimeMs, principalName,
-                extensions, (UIntPtr) extensions.Length,
+                extensions, (UIntPtr) (extensions?.Length ?? 0),
                 errorStringBuilder, (UIntPtr) errorStringBuilder.Capacity);
 
             if (errorCode != ErrorCode.NoError)
