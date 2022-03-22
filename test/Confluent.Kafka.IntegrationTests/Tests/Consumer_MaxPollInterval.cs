337a8fc539766eff7fff5f94c0d26c31ff869fad
Soren Schwartz <s_schwartz@live.dk>
2022-03-22 21:22:54 +0100

    Add integration test which validates consumer doesn't freeze from within log handler

---
+++ test/Confluent.Kafka.IntegrationTests/Tests/Consumer_MaxPollInterval.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace Confluent.Kafka.IntegrationTests
+{
+    public partial class Tests
+    {
+        /// <summary>
+        ///  Validates consumer able to use API's after
+        ///  MaxPollInterval exceeded.
+        /// </summary>
+        [Theory(Timeout=30000), MemberData(nameof(KafkaParameters))]
+        public void Consumer_MaxPollInterval(string bootstrapServers)
+        {
+            LogToFile("start Consumer_MaxPollInterval");
+
+            var sessionTimeoutMs = TimeSpan.FromSeconds(6);
+            var maxPollIntervalMs = sessionTimeoutMs.Add(TimeSpan.FromMilliseconds(1));
+
+            Util.ProduceNullStringMessages(bootstrapServers, singlePartitionTopic, 100, 2);
+
+            var consumerConfig = new ConsumerConfig
+            {
+                BootstrapServers = bootstrapServers,
+                GroupId = Guid.NewGuid().ToString(),
+                MaxPollIntervalMs = (int)maxPollIntervalMs.TotalMilliseconds,
+                SessionTimeoutMs = (int)sessionTimeoutMs.TotalMilliseconds,
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            var maxPolled = 0;
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig)
+                   .SetLogHandler(((consumerWithin, message) =>
+                   {
+                       if (message.Facility.Equals("MAXPOLL"))
+                       {
+                           LogToFile("Max poll interval exceeded");
+                           maxPolled = 1;
+                           // This should not freeze
+                           var memberId = consumerWithin.MemberId;
+                       }
+                   }))
+                   .Build())
+            {
+                consumer.Subscribe(singlePartitionTopic);
+                var count = 0;
+                while (true)
+                {
+                    if(count == 0)
+                    {
+                        var cr = consumer.Consume(TimeSpan.FromSeconds(10));
+                        if (cr == null) { continue; }
+                        if (count == 0)
+                        {
+                            Thread.Sleep(maxPollIntervalMs.Add(TimeSpan.FromSeconds(5)));
+                        }
+                    } else
+                    {
+                        Assert.Throws<ConsumeException>(() => consumer.Consume(TimeSpan.FromSeconds(10)));
+                        break;
+                    }
+                }
+            }
+
+            Assert.Equal(1, maxPolled);
+            Assert.Equal(0, Library.HandleCount);
+            LogToFile("end Consumer_MaxPollInterval");
+        }
+    }
+}
