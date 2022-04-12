daa997de31d753593b444de0e782cf9224ec2ed7
Matt Howlett <matt@confluent.io>
2022-04-12 10:09:36 -0500

    Revert Config_gen.cs

--- src/Confluent.Kafka/Config_gen.cs
+++ src/Confluent.Kafka/Config_gen.cs
@@ -1,4 +1,4 @@
-// *** Auto-generated from librdkafka v1.9.0-RC2b *** - do not modify manually.
+// *** Auto-generated from librdkafka v1.9.0-RC2 *** - do not modify manually.
 //
 // Copyright 2018 Confluent Inc.
 //
