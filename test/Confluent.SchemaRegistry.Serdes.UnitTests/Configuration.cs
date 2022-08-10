ff3c3d050730d2c30ba4fb60eb8c7b8c47c1f985
Robert Yokota <rayokota@gmail.com>
2022-08-10 14:45:55 -0700

    Fix tests

--- test/Confluent.SchemaRegistry.Serdes.UnitTests/Configuration.cs
+++ test/Confluent.SchemaRegistry.Serdes.UnitTests/Configuration.cs
@@ -33,8 +33,8 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
         {
             testTopic = "topic";
             var schemaRegistryMock = new Mock<ISchemaRegistryClient>();
-            schemaRegistryMock.Setup(x => x.RegisterSchemaAsync(testTopic + "-value", It.IsAny<Schema>(), false)).ReturnsAsync(
-                (string topic, string schema) => store.TryGetValue(schema, out int id) ? id : store[schema] = store.Count + 1
+            schemaRegistryMock.Setup(x => x.RegisterSchemaAsync(testTopic + "-value", It.IsAny<Schema>(), It.IsAny<bool>())).ReturnsAsync(
+                (string topic, string schema, bool normalize) => store.TryGetValue(schema, out int id) ? id : store[schema] = store.Count + 1
             );
             schemaRegistryMock.Setup(x => x.GetSchemaAsync(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(
                 (int id) => new Schema(store.Where(x => x.Value == id).First().Key, null, SchemaType.Avro)
