dfc91e98c1b5cbfeb02b84f2353da927cdfd07b8
lin.qingliang <chad122@163.com>
2023-04-11 17:59:49 +0800

    add ConfluentKafkaTraceEventDic

--- src/Confluent.Kafka/Internal/Diagnostic/ConfluentKafkaTrace.cs
+++ src/Confluent.Kafka/Internal/Diagnostic/ConfluentKafkaTrace.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
 using System.Reflection;
 
 namespace Confluent.Kafka.Internal.Diagnostic
@@ -15,10 +16,11 @@ namespace Confluent.Kafka.Internal.Diagnostic
         public static SourceSwitch Switch => Source.Switch;
 #endif
 
+        private static IDictionary<ConfluentKafkaTraceEvent, string> ConfluentKafkaTraceEventDic => Enum.GetNames(typeof(ConfluentKafkaTraceEvent)).ToDictionary(x => (ConfluentKafkaTraceEvent)Enum.Parse(typeof(ConfluentKafkaTraceEvent), x));
         internal static void TraceEvent(ConfluentKafkaTraceEvent confluentKafkaTraceEvent, params object[] args)
         {
 #if !NETSTANDARD1_3
-            Source.TraceEvent(TraceEventType.Information, (int)confluentKafkaTraceEvent, confluentKafkaTraceEvent.ToString(), args);
+            Source.TraceEvent(TraceEventType.Information, (int)confluentKafkaTraceEvent, ConfluentKafkaTraceEventDic[confluentKafkaTraceEvent], args);
 #endif
         }
     }
