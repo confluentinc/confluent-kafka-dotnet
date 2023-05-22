6ece0c333b431f2b2cb63d68d3b34a1599eb4fd0
jainruchir <rujain@confluent.io>
2023-05-22 13:51:07 +0530

    added unit test for schema w json ref SerDes

--- test/Confluent.SchemaRegistry.Serdes.UnitTests/JsonSerializeDeserialize.cs
+++ test/Confluent.SchemaRegistry.Serdes.UnitTests/JsonSerializeDeserialize.cs
@@ -20,6 +20,7 @@
 using Confluent.Kafka;
 using Moq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using NJsonSchema.Generation;
 using System;
@@ -35,6 +36,53 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
 {
     public class JsonSerializeDeserialzeTests
     {
+        public string schema1 = @"
+{
+  ""$schema"": ""http://json-schema.org/draft-07/schema#"",
+  ""title"": ""Schema1"",
+  ""$id"": ""schema1.json"",
+  ""type"": ""object"",
+  ""properties"": {
+    ""field1"": {
+      ""type"": ""string""
+    },
+    ""field2"": {
+      ""type"": ""integer""
+    },
+    ""field3"": {
+      ""$ref"": ""schema2.json#/definitions/field""
+    }
+  }
+}
+";
+        public string schema2 = @"
+{
+  ""$schema"": ""http://json-schema.org/draft-07/schema#"",
+  ""$id"": ""schema2.json"",
+  ""title"": ""Schema2"",
+  ""type"": ""object"",
+  ""definitions"": {
+    ""field"": {
+      ""type"": ""boolean""
+    }
+  }
+}
+";
+        public class BoolValue
+        {
+            public bool Value { get; set; }
+        }
+        public static Func<string, BoolValue> Convertor = (json) =>
+        {
+            JObject obj = JObject.Parse(json);
+            bool value = (bool)obj["field3"];
+
+            BoolValue boolValue = new BoolValue
+            {
+                Value = value
+            };
+            return boolValue;
+        };
         public class UInt32Value
         {
             public int Value { get; set; }
@@ -96,6 +144,7 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
         }
 
         private ISchemaRegistryClient schemaRegistryClient;
+        private ISchemaRegistryClient schemaRegistryClientJsonRef;
         private string testTopic;
         private Dictionary<string, int> store = new Dictionary<string, int>();
 
@@ -110,7 +159,29 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
             schemaRegistryMock.Setup(x => x.GetSchemaAsync(It.IsAny<int>(), It.IsAny<string>())).ReturnsAsync(
                 (int id, string format) => new Schema(store.Where(x => x.Value == id).First().Key, null, SchemaType.Protobuf)
             );
-            schemaRegistryClient = schemaRegistryMock.Object;   
+            schemaRegistryClient = schemaRegistryMock.Object;
+
+            var schemaRegistryMockJsonRef = new Mock<ISchemaRegistryClient>();
+            schemaRegistryMockJsonRef.Setup(x => x.RegisterSchemaAsync("topic-Schema2", It.IsAny<Schema>(), It.IsAny<bool>())).ReturnsAsync(
+                (string topic, Schema schema, bool normalize) => store.TryGetValue(schema.SchemaString, out int id) ? id : store[schema.SchemaString] = store.Count + 1
+            );
+            schemaRegistryMockJsonRef.Setup(x => x.RegisterSchemaAsync("topic-Schema1", It.IsAny<Schema>(), It.IsAny<bool>())).ReturnsAsync(
+                (string topic, Schema schema, bool normalize) => store.TryGetValue(schema.SchemaString, out int id) ? id : store[schema.SchemaString] = store.Count + 1
+            );
+            schemaRegistryMockJsonRef.Setup(x => x.GetLatestSchemaAsync("topic-Schema2"))
+            .ReturnsAsync((string subject) => new RegisteredSchema("topic-Schema2", 1, store.TryGetValue(schema2, out int id) ? id : store[schema2] = store.Count + 1, schema2, SchemaType.Json, new List<SchemaReference>()));
+            var refs = new List<SchemaReference> { new SchemaReference("schema2.json", "topic-Schema2", 1) };
+            schemaRegistryMockJsonRef.Setup(x => x.GetLatestSchemaAsync("topic-Schema1"))
+            .ReturnsAsync((string subject) => new RegisteredSchema("topic-Schema1", 1, store.TryGetValue(schema1, out int id) ? id : store[schema1] = store.Count + 1, schema1, SchemaType.Json, refs));
+            schemaRegistryMockJsonRef.Setup(x => x.GetRegisteredSchemaAsync("topic-Schema2", It.IsAny<int>()))
+            .ReturnsAsync((string subject, int version) =>
+                new RegisteredSchema("topic-Schema2", version,
+                    store.TryGetValue(schema2, out int id) ? id : store[schema2] = store.Count + 1, schema2, SchemaType.Json, new List<SchemaReference>())
+            );
+            schemaRegistryMockJsonRef.Setup(x => x.GetRegisteredSchemaAsync("topic-Schema1", It.IsAny<int>()))
+            .ReturnsAsync((string subject, int version) => new RegisteredSchema("topic-Schema1", version, store.TryGetValue(schema1, out int id) ? id : store[schema1] = store.Count + 1, schema1, SchemaType.Json, refs)
+            );
+            schemaRegistryClientJsonRef = schemaRegistryMockJsonRef.Object;
         }
 
         [Fact]
@@ -166,6 +237,41 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
             Assert.Equal(v.Value, actual.Value);
         }
 
+        [Fact]
+        public async Task WithJsonSchemaExternalReferencesAsync()
+        {
+            var subject1 = $"{testTopic}-Schema1";
+            var subject2 = $"{testTopic}-Schema2";
+
+            var id2 = schemaRegistryClientJsonRef.RegisterSchemaAsync(subject2, new Schema(schema2, Confluent.SchemaRegistry.SchemaType.Json)).Result;
+            var s2 = schemaRegistryClientJsonRef.GetLatestSchemaAsync(subject2).Result;
+            var refs = new List<SchemaReference> { new SchemaReference("schema2.json", subject2, s2.Version) };
+            var id1 = schemaRegistryClientJsonRef.RegisterSchemaAsync(subject1, new Schema(schema1, refs, Confluent.SchemaRegistry.SchemaType.Json)).Result;
+            var s1 = schemaRegistryClientJsonRef.GetLatestSchemaAsync(subject1).Result;
+            var unreg_schema1 = s1.Schema;
+            var jsonSerializerConfig = new JsonSerializerConfig
+            {
+                UseLatestVersion = true,
+                AutoRegisterSchemas = false,
+                SubjectNameStrategy = SubjectNameStrategy.TopicRecord
+            };
+            var jsonSerializer = new NonGenericJsonSerializer(schemaRegistryClientJsonRef, unreg_schema1, jsonSerializerConfig);
+            var jsonDeserializer = new JsonDeserializer<BoolValue>(schemaRegistryClientJsonRef, unreg_schema1, Convertor: Convertor);
+            var v = new
+            {
+                field1 = "Hello",
+                field2 = 123,
+                field3 = true
+            };
+            string expectedJson = "{\"field1\":\"Hello\",\"field2\":123,\"field3\":true}";
+            var bytes = await jsonSerializer.SerializeAsync(v, new SerializationContext(MessageComponentType.Value, testTopic));
+            Assert.NotNull(bytes);
+            Assert.Equal(expectedJson, Encoding.UTF8.GetString(bytes.AsSpan().Slice(5)));
+
+            var actual = await jsonDeserializer.DeserializeAsync(bytes, false, new SerializationContext(MessageComponentType.Value, testTopic));
+            Assert.Equal(v.field3, actual.Value);
+        }
+
         [Theory]
         [InlineData(EnumHandling.CamelCaseString, EnumType.EnumValue, "{\"Value\":\"enumValue\"}")]
         [InlineData(EnumHandling.String, EnumType.None, "{\"Value\":\"None\"}")]
@@ -217,8 +323,8 @@ namespace Confluent.SchemaRegistry.Serdes.UnitTests
         {
             var jsonSerializer = new JsonSerializer<NonNullStringValue>(schemaRegistryClient);
 
-            var v = new NonNullStringValue 
-            { 
+            var v = new NonNullStringValue
+            {
                 Nested = new()
                 {
                     Value = null
