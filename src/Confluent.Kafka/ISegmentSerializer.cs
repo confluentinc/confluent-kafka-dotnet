c5c0758bce47e74c1035f1103672f7d291766814
neil <neil.mosafi@mavensecurities.com>
2024-01-22 22:18:39 +0000

    Add a new type of Serializer for supporting zero allocation scenarios
    Right now `ISerializer` and `ISerializerAsync` are forced to allocate a new array for every message which is sent. For a high throughput application we'd like to avoid allocating large amounts of memory.
    
    The API exposed by librdkafka supports buffer reuse as it accepts a length and offset alongside each byte array, however this is not exposed by the serialization framework in Kafka dotnet.
    
    The simplest approach here is to allow `ArraySegment<byte>` to be returned by the serializer rather than `byte[]`, however changing all serializers to return `ArraySegment<byte>`` would surely be a large breaking change. So instead I decided to implement this as a new type of serializer.

---
+++ src/Confluent.Kafka/ISegmentSerializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Confluent.Kafka
+{
+    /// <summary>
+    ///     Defines a serializer for use with <see cref="Confluent.Kafka.Producer{TKey,TValue}" />.
+    /// </summary>
+    public interface ISegmentSerializer<T>
+    {
+        /// <summary>
+        ///     Serialize the key or value of a <see cref="Message{TKey,TValue}" />
+        ///     instance.
+        /// </summary>
+        /// <param name="data">
+        ///     The value to serialize.
+        /// </param>
+        /// <param name="context">
+        ///     Context relevant to the serialize operation.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="ArraySegment{T}"/> containing the serialized value.
+        /// </returns>
+        ArraySegment<byte> Serialize(T data, SerializationContext context);
+    }
+}
