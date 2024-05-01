c6c35e619a38a9a728aa98afd4bcfa76b3d01dcb
Emanuele Sabellico <esabellico@confluent.io>
2024-05-01 10:51:58 +0200

    Test 2

--- test/Confluent.Kafka.IntegrationTests/Tests/Consumer_Commit_Committed_Position.cs
+++ test/Confluent.Kafka.IntegrationTests/Tests/Consumer_Commit_Committed_Position.cs
@@ -17,6 +17,7 @@
 #pragma warning disable xUnit1026
 
 using System;
+using System.Threading;
 using System.Collections.Generic;
 using Xunit;
 using Confluent.Kafka.TestsCommon;
@@ -51,22 +52,41 @@ namespace Confluent.Kafka.IntegrationTests
 
             var firstMessage = messages[0];
             var lastMessage = messages[N - 1];
+
             using (var consumer = new TestConsumerBuilder<byte[], byte[]>(consumerConfig).Build())
             {
-                if (TestConsumerGroupProtocol.IsClassic())
-                    consumer.Assign(new TopicPartitionOffset(singlePartitionTopic, 0, firstMsgOffset));
-                else
-                    // FIXME: There are problems with new coordinator when
-                    // manually assigning and committing offsets for a group
-                    // id it's returning NotCoordinatorForGroup
-                    consumer.Subscribe(singlePartitionTopic);
+                consumer.Assign(new TopicPartitionOffset(singlePartitionTopic, 0, firstMsgOffset));
                 // Test #0.5 (invalid cases)
                 var offset = consumer.Position(new TopicPartition("invalid-topic", 0));
                 Assert.Equal(Offset.Unset, offset);
 
                 // Test #1
+                // This is one of the first tests, it seems with KRaft
+                // group coordinator is loaded on demand.
                 var record = consumer.Consume(TimeSpan.FromMilliseconds(30000));
-                var os = consumer.Commit();
+                List<TopicPartitionOffset> os = null;
+                while (os == null)
+                {
+                    try
+                    {
+                        os = consumer.Commit();
+                    }
+                    catch (KafkaException e)
+                    {
+                        Console.WriteLine(e.Error);
+                        if (e.Error == ErrorCode.GroupLoadInProgress ||
+                            e.Error == ErrorCode.NotCoordinatorForGroup)
+                        {
+                            Thread.Sleep(1000);
+                            continue;
+                        }
+                        else
+                        {
+                           throw;
+                        }
+                    }
+                }
+
                 Assert.Equal(firstMsgOffset + 1, os[0].Offset);
                 offset = consumer.Position(new TopicPartition(singlePartitionTopic, 0));
                 var co = consumer.Committed(new List<TopicPartition> { new TopicPartition(singlePartitionTopic, 0) }, TimeSpan.FromSeconds(10));
