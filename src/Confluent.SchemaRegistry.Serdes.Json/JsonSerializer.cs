34f34280d2a6c63449ffcae0d2d7ea748cb3ad78
Robert Yokota <rayokota@gmail.com>
2024-05-29 17:49:08 -0700

    Fix merge issue

--- src/Confluent.SchemaRegistry.Serdes.Json/JsonSerializer.cs
+++ src/Confluent.SchemaRegistry.Serdes.Json/JsonSerializer.cs
@@ -106,6 +106,7 @@ namespace Confluent.SchemaRegistry.Serdes
             if (config.AutoRegisterSchemas != null) { this.autoRegisterSchema = config.AutoRegisterSchemas.Value; }
             if (config.NormalizeSchemas != null) { this.normalizeSchemas = config.NormalizeSchemas.Value; }
             if (config.UseLatestVersion != null) { this.useLatestVersion = config.UseLatestVersion.Value; }
+            if (config.LatestCompatibilityStrict != null) { this.latestCompatibilityStrict = config.LatestCompatibilityStrict.Value; }
             if (config.UseLatestWithMetadata != null) { this.useLatestWithMetadata = config.UseLatestWithMetadata; }
             if (config.SubjectNameStrategy != null) { this.subjectNameStrategy = config.SubjectNameStrategy.Value.ToDelegate(); }
 
