569917b1eb484165f67cfa60195403d32ceb82ee
Robert Yokota <rayokota@gmail.com>
2025-08-19 10:06:16 -0700

    Support data quality and schema migration rules for net462 (#2514)

--- src/Confluent.SchemaRegistry.Rules/BuiltinLibrary.cs
+++ src/Confluent.SchemaRegistry.Rules/BuiltinLibrary.cs
@@ -7,15 +7,15 @@ namespace Confluent.SchemaRegistry.Rules
 
         public virtual IList<EnvOption> CompileOptions
         {
-            get => new List<EnvOption> { IEnvOption.Declarations(BuiltinDeclarations.Create()) };
+            get => new List<EnvOption> { EnvOptions.Declarations(BuiltinDeclarations.Create()) };
         }
 
         public virtual IList<ProgramOption> ProgramOptions
         {
             get => new List<ProgramOption>
             {
-                IProgramOption.EvalOptions(EvalOption.OptOptimize),
-                IProgramOption.Functions(BuiltinOverload.Create())
+                Cel.ProgramOptions.EvalOptions(EvalOption.OptOptimize),
+                Cel.ProgramOptions.Functions(BuiltinOverload.Create())
             };
         }
     }
