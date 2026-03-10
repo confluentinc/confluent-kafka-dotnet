6262fc1230564e0091fffa39a3bbcf3b7c28b866
mohammed-saalim <mohammed.saalim.k@gmail.com>
2026-03-10 16:19:17 -0500

    Add [Obsolete] attribute to deprecated ListGroups and ListGroup methods (#1998)

--- src/Confluent.Kafka/IAdminClient.cs
+++ src/Confluent.Kafka/IAdminClient.cs
@@ -40,6 +40,7 @@ namespace Confluent.Kafka
         /// <param name="timeout">
         ///     The maximum period of time the call may block.
         /// </param>
+        [Obsolete("Superseded by ListConsumerGroupsAsync and DescribeConsumerGroupsAsync. This method will be removed in a future release.")]
         List<GroupInfo> ListGroups(TimeSpan timeout);
 
 
@@ -64,6 +65,7 @@ namespace Confluent.Kafka
         ///     specified group or null if this group does
         ///     not exist.
         /// </returns>
+        [Obsolete("Superseded by ListConsumerGroupsAsync and DescribeConsumerGroupsAsync. This method will be removed in a future release.")]
         GroupInfo ListGroup(string group, TimeSpan timeout);
 
 
